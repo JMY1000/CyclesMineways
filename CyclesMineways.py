e4d4f5affec1db2138b3b8263e9399ec8913b819
JMY1000 <JMY1000@users.noreply.github.com>
2016-04-10 14:30:47 -0700

    Changed copyright

--- CyclesMineways.py
+++ CyclesMineways.py
@@ -1,6 +1,6 @@
 # Cycles Mineways setup
 # Version 1.2.2, 4/10/16
-# Copyright © 2016 Jonathan Edelman
+# Copyright © 2016
 # Please send suggestions or report bugs at https://github.com/JMY1000/CyclesMineways/
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation under version 3 of the License.
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
